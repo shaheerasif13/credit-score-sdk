9e5ee13a490cd3d82e4a01602e933b8e26ab17d5
Muhammad Shaheer <shaheerasif13@outlook.com>
2025-01-31 21:36:31 +0500

    feat: Enhance NumberInput component with theme-based border color handling

--- src/components/NumberInput.tsx
+++ src/components/NumberInput.tsx
@@ -1,3 +1,5 @@
+import { useTheme } from "../hooks/theme";
+
 interface NumberInputProps {
   label: string;
   min: number;
@@ -5,6 +7,8 @@ interface NumberInputProps {
 }
 
 const NumberInput: React.FC<NumberInputProps> = ({ label, min, onChange }) => {
+  const { primaryColor } = useTheme();
+
   return (
     <div>
       <label className="block text-sm font-medium text-gray-700 mb-1.5">
@@ -14,7 +18,12 @@ const NumberInput: React.FC<NumberInputProps> = ({ label, min, onChange }) => {
         type="number"
         min={min}
         onChange={(e) => onChange(Number(e.target.value))}
-        className="w-full p-2 border-2 border-gray-400 focus:border-gray-500 text-gray-700 rounded-lg text-sm focus:outline-none"
+        className="w-full p-2 border-2 text-gray-700 rounded-lg text-sm focus:outline-none"
+        style={{
+          borderColor: "#9ca3af",
+        }}
+        onFocus={(e) => (e.target.style.borderColor = primaryColor)}
+        onBlur={(e) => (e.target.style.borderColor = "#9ca3af")}
       />
     </div>
   );
